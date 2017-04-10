f33f17ac6553f54a061adc62e5c23e2179a6efa8
Chris Gross <chris.gross@hellolabs.com>
2017-04-10 01:14:47 -0400

    force numeric sequential variables if they are present

--- app/models/black_card.rb
+++ app/models/black_card.rb
@@ -1,4 +1,23 @@
 class BlackCard < ApplicationRecord
 	validates :text, presence: true, uniqueness: true
+	validate :vars_are_numeric, :vars_are_ordered
 	belongs_to :pack
+
+	def vars_are_numeric
+		return unless text
+		text.match(/\{[^\}]{2,}\}/) { |m| errors.add(:text, "#{m} is not valid") }
+		text.match(/\{[^1-3\}]*\}/) { |m| errors.add(:text, "#{m} is not valid") }
+	end
+
+	def vars_are_ordered
+		return unless text
+		vars = text.scan(/\{([1-3])\}/).flatten
+		return unless vars.length > 0
+		unless vars[0] == "1"
+			errors.add(:text, "first variable must be 1")
+		end
+		unless vars.each_cons(2).all? { |a,b| a.to_i+1 == b.to_i }
+			errors.add(:text, "variables must be in sequential order")
+		end
+	end
 end
--- spec/models/black_card_spec.rb
+++ spec/models/black_card_spec.rb
@@ -15,6 +15,32 @@ RSpec.describe BlackCard, type: :model do
       black_card = FactoryGirl.build(:black_card)
       expect(black_card).not_to be_valid
     end
+    describe "text with variables" do
+      it "is valid with variables" do
+        @black_card.text = "{1} me the {2}, {3}"
+        expect(@black_card).to be_valid
+      end
+      it "is not valid with bad variable name" do
+        @black_card.text = "This has an invalid variable, {i}"
+        expect(@black_card).not_to be_valid
+      end
+      it "is not valid with out of sequence variables" do
+        @black_card.text = "These {2} variables {1} are not in order"
+        expect(@black_card).not_to be_valid
+      end
+      it "is not valid with gaps in variable numbers" do
+        @black_card.text = "{1} cannot skip to {3}"
+        expect(@black_card).not_to be_valid
+      end
+      it "is not valid if first variable is not 1" do
+        @black_card.text = "{2} cannot be the first"
+        expect(@black_card).not_to be_valid
+      end
+      it "is not valid with more than three variables" do
+        @black_card.text = "{1} {2} {3} {4} has too many variables"
+        expect(@black_card).not_to be_valid
+      end
+    end
   end
 
   describe "associations" do
